5dad812a2549faff47f78a8a7a8c921a4f31c675
Daniel Sadiq <danielsadiq93@gmail.com>
2025-05-29 18:51:14 +0100

    challenge start

--- linked-lisk.py
+++ linked-lisk.py
@@ -94,6 +94,16 @@ class LinkedList:
       itr = itr.next
       count +=1
 
+  def insert_after_value(self, data_after, data_to_insert):
+    # Search for first occurance of data_after value in linked list
+    itr = self.head
+    # Now insert data_to_insert after data_after node
+    pass
+
+  def remove_by_value(self, data):
+    # Remove first node that contains data
+    pass
+
       
 
 ll = LinkedList()
